019b7d1f317a96f29190032de5d96f7ce6a917f0
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-08-29 11:59:17 +0200

    Oppdaterer url til dittnav-api

--- src/api/urls.js
+++ src/api/urls.js
@@ -15,8 +15,8 @@ export const getEnvironment = () => {
 
 const OPPFOLGING_URL = {
   local: "http://localhost:3000/api/oppfolging",
-  development: "https://person.dev.nav.no/dittnav-api/oppfolging",
-  production: "https://person.nav.no/dittnav-api/oppfolging",
+  development: "https://www.dev.nav.no/dittnav-api/oppfolging",
+  production: "https://www.nav.no/dittnav-api/oppfolging",
 };
 
 const STILLINGER_URL = {
