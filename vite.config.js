b4aed2cb312908f200da6483d4e1f00a8ea7c3ac
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-01-16 21:44:42 +0100

    Manifest fil som kan brukes for statiske assets på CDN

--- vite.config.js
+++ vite.config.js
@@ -41,6 +41,7 @@ export default ({ command }) => ({
     }),
   ],
   build: {
+    manifest: true,
     rollupOptions: {
       input: resolve(__dirname, "src/Mikrofrontend.jsx"),
       preserveEntrySignatures: "exports-only",
