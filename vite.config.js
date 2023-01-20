1431140e01025a9b370a404c084381115019491b
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-01-20 13:41:21 +0100

    Deploy bundle assets til cdn

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { viteMockServe } from "vite-plugin-mock";
 import { rollupImportMapPlugin } from "rollup-plugin-import-map";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import EnvironmentPlugin from "vite-plugin-environment";
-import viteCompression from "vite-plugin-compression";
 import { terser } from "rollup-plugin-terser";
 import { resolve } from "path";
 
@@ -19,12 +18,6 @@ const imports = {
 export default ({ command }) => ({
   plugins: [
     react(),
-    viteCompression({
-      algorithm: "gzip",
-    }),
-    viteCompression({
-      algorithm: "brotliCompress",
-    }),
     viteMockServe({
       mockPath: "mock",
       localEnabled: command === "serve",
